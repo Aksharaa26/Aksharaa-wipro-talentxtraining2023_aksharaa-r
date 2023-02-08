2d7c8c4a25684c13cf344f519760972c11aa2be6
Aksharaa26 <90827612+Aksharaa26@users.noreply.github.com>
2023-02-08 07:10:36 +0530

    get code through strings.java

---
+++ Day 1/get code through strings.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+class UserMainCode{
+public int getCodeThroughString(String
+input1){
+String word[=input1.split(" ");
+int sum=0;
+for(int i=0;i<word.length;i++)
+{
+sum+=word i].length0;
+}
+return (1 + (sum-1) %9);
+}}
